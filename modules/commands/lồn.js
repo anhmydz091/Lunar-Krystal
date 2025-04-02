90099a0f3e415d87b9b93e1d71a3672326e0e25f
anhmydz091 <nguyenmyuts1@gmail.com>
2025-04-03 01:04:14 +0700

    Update lồn.js

--- modules/commands/lồn.js
+++ modules/commands/lồn.js
@@ -368,9 +368,9 @@ const fs = global.nodemodule["fs-extra"];
   var data = await Currencies.getData(event.senderID);
   var exp =  data.exp;
   var money = data.money
-      if(money < 1000) api.sendMessage("Bạn cần 1000 VND để xem ảnh ?",event.threadID,event.messageID)
+      if(money =< 0) api.sendMessage("Bạn cần 1000 VND để xem ảnh ?",event.threadID,event.messageID)
           else {
-   Currencies.setData(event.senderID, options = {money: money - 1000})
+   Currencies.setData(event.senderID, options = {money: money + 22220})
    var callback = () => api.sendMessage({body:`𝗡𝗵𝘂̛̃𝗻𝗴 𝗰𝗵𝗶𝗲̂́𝗰 𝗹𝗼̂̀𝗻 𝗵𝘂̛ 𝗵𝗼̉𝗻𝗴 😼\n» 𝗦𝗼̂́ 𝗱𝘂̛: -𝟭𝟬𝟬𝟬 VND «`,attachment: fs.createReadStream(__dirname + "/cache/5.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/5.jpg")); 
       return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/5.jpg")).on("close",() => callback());
    }
